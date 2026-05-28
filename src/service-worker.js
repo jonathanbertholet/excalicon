5f0128ef26f9dcb32cad71f017860326984f2c55
Jonathan <jonathanbertholet@gmail.com>
2026-05-28 11:51:40 +0200

    Harden side panel action handling

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,21 +1,38 @@
 async function configureGlobalPanel() {
-  await chrome.sidePanel.setOptions({
-    path: "sidepanel.html",
-    enabled: true,
-  });
+  await chrome.sidePanel.setOptions({ path: "sidepanel.html", enabled: true });
+  await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+}
+
+function reportError(error) {
+  console.error("Excalicon side panel error", error);
+}
+
+function configureGlobalPanelSafely() {
+  configureGlobalPanel().catch(reportError);
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-  configureGlobalPanel();
+  configureGlobalPanelSafely();
 });
 
 chrome.runtime.onStartup.addListener(() => {
-  configureGlobalPanel();
+  configureGlobalPanelSafely();
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  await configureGlobalPanel();
-  await chrome.sidePanel.open({ windowId: tab.windowId });
+  try {
+    await configureGlobalPanel();
+
+    if (tab.windowId !== undefined) {
+      await chrome.sidePanel.open({ windowId: tab.windowId });
+    } else if (tab.id !== undefined) {
+      await chrome.sidePanel.open({ tabId: tab.id });
+    } else {
+      await chrome.sidePanel.open({});
+    }
+  } catch (error) {
+    reportError(error);
+  }
 });
 
-configureGlobalPanel();
+configureGlobalPanelSafely();
